b94c0c28d0d521d612d64220c6a59b8eff8b7592
Eshaan Singh Parihar <eshaansinghparihar@esh.local>
2023-11-30 17:00:18 +0530

    minor changes

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addTodo } from '../features/todoSlice'
+import { PlusCircleIcon } from '@heroicons/react/24/outline'
 
 export default function AddTodo(){
   const [todo,setTodo] = useState('')
@@ -16,12 +17,14 @@ export default function AddTodo(){
     setTodo('')
   }
   return (
-    <div className='m-12'>
+    <div className='flex gap-5 items-center	justify-items-center'>
         <input placeholder='Enter todo here' 
         className='m-4 w-4/5 p-4 text-2xl'
         value={todo}
         onChange={handleTodo}/>
-        <button onClick={handleAddTodo}>Add Todo</button>
+        <button onClick={handleAddTodo}>
+        <PlusCircleIcon className='h-9 w-9 text-blue-600'/>
+        </button>
     </div>
   )
 }
